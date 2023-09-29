0c7b145ceec5fa01a0755ee8e572a39e43d1be90
glebychyasher <zobningleb01@icloud.com>
2023-09-29 18:00:07 +0300

    Ready

--- AppLesson4Colours/ViewController.swift
+++ AppLesson4Colours/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
+    
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    
+    @IBOutlet weak var paletteView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paletteView.layer.cornerRadius = 25
+        setInitialOutlets()
+        setPaletteColour()
     }
 
-
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSliderOutlet.value)
+        setPaletteColour()
+    }
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSliderOutlet.value)
+        setPaletteColour()
+    }
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
+        setPaletteColour()
+    }
+}
+//MARK: Set the colour of palelle (paletteView)
+extension ViewController {
+    private func setPaletteColour() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSliderOutlet.value),
+            green: CGFloat(greenSliderOutlet.value),
+            blue: CGFloat(blueSliderOutlet.value),
+            alpha: 1
+        )
+    }
 }
 
+extension ViewController {
+    private func setInitialOutlets() {
+        redValueLabel.text = String(format: "%.2f", redSliderOutlet.value)
+        greenValueLabel.text = String(format: "%.2f", greenSliderOutlet.value)
+        blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
+    }
+}
